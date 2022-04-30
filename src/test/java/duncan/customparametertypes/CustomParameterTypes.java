70f812aa954495ef142be7cb41637854b649bf7d
Michael Duncan <mwduncan2018@gmail.com>
2022-04-30 17:19:10 -0400

    Updates

--- src/test/java/duncan/customparametertypes/CustomParameterTypes.java
+++ src/test/java/duncan/customparametertypes/CustomParameterTypes.java
@@ -4,6 +4,13 @@ import io.cucumber.java.ParameterType;
 
 public class CustomParameterTypes {
 
+	/*
+	 * Custom parameter types only take one parameter.
+	 * To reduce complexity, pass a string instead.
+	 * Do not use custom parameter types.
+	 * They seem like a good idea on the surface, but introduce extra complexity with little benefit.
+	 */
+	
 	@ParameterType(".*")
 	public Product product(String str) {
 		Product product = new Product();
